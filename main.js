bc54bc825400ac92335a5b4775e29a98c565f187
unknown <shubhamrangra80@gmail.com>
2024-02-19 22:17:51 +1300

    mobile compatible

--- main.js
+++ main.js
@@ -1,11 +1,4 @@
 
-
-const outer_circle = document.getElementById("outer-circle");
-const iner_circle = document.getElementById("iner-circle");
-const theme_button = document.getElementById("iner-iner-circle");
-const theme_id = document.getElementById("theme_mode");
-
-//Buttons
 const btn_minimze = document.getElementById("minimize");
 const btn_resize = document.getElementById("resize");
 const btn_close = document.getElementById("close");
@@ -13,60 +6,11 @@ var minimize_click = false;
 var resize_click = false;
 var close_click = false;
 
-var outer = false;
-var inner = false;
-var theme = true;
-
-outer_circle.addEventListener("mouseover", () =>{
-    outer = true;
-
-    if(!inner){
-        outer_circle.style.backgroundColor = "#292929";
+function checkDevice(){
+    if(window.matchMedia("max-width: 768px").matches){
+        btn_minimze.disabled = true;
     }
-    if(inner){
-        outer_circle.style.backgroundColor = "#121212";
-    }
-});
-
-iner_circle.addEventListener("mouseover", () =>{
-    inner = true;
-    iner_circle.style.backgroundColor = "#424242"
-});
-outer_circle.addEventListener("mouseout", ()=>{
-    outer = false;
-    outer_circle.style.backgroundColor = "#121212";
-});
-iner_circle.addEventListener("mouseout", ()=>{
-    inner = false;
-    iner_circle.style.backgroundColor = "#212121"
-});
-
-theme_button.addEventListener("click", ()=>{
-    theme = !theme
-    if(theme){
-        document.querySelector("body").style.backgroundColor = "#378729";
-        document.querySelector(".window").style.backgroundColor = "#d6d6d6";
-
-        document.querySelectorAll("#theme_mode li a, #theme_mode h1").forEach(element => {
-            element.style.color = "#fff"
-        });
-        document.querySelector(".info").style.color = "#000"
-        
-    }else{
-        document.querySelector(".window").style.backgroundColor = "#121212";
-        document.querySelectorAll("#theme_mode li a, #theme_mode h1").forEach(element => {
-            element.style.color ="#000";
-        });
-        document.querySelector(".info").style.color = "#fff"
-    }
-    theme_button.disabled = true;
-    // setTimeout(()=>{
-        
-    // },2000);
-    setTimeout(()=>{
-        theme_button.disabled = false;
-    },1000)
-});
+}
 
 
 btn_minimze.addEventListener("click", ()=>{
@@ -83,12 +27,21 @@ btn_minimze.addEventListener("click", ()=>{
 
 btn_resize.addEventListener("click", ()=>{
     if(!resize_click){
-        document.querySelector(".window").style.width = "1200px";
-        document.querySelector(".window").style.height = "900px";
-        
+        document.querySelector(".window").style.width = "70%";
+        document.querySelector(".window").style.height = "85%";
+        document.querySelector(".window-footer").style.width = "50%";
+        document.querySelector(".window-footer").style.height = "10%";
+        window.location.hash = "#main";
+        const targetElement = document.getElementById("window");
+        targetElement.scrollIntoView({behavior: "smooth", block: "start"});
+        resize_click = !resize_click; 
     }
     else{
-        document.querySelector(".window").style.opacity = "900px";
-        document.querySelector(".window").style.height = "600px"
+        document.querySelector(".window").style.width = "50%";
+        document.querySelector(".window").style.height = "70%";
+        document.querySelector(".window-footer").style.width = "35%";
+        document.querySelector(".window-footer").style.height = "10%";
+        resize_click = !resize_click;
     }
 });
+
